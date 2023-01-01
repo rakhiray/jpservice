37905c94c7bb39fc600ae415a3c40630dbd3f054
rakhi ray <rakhiiray14@gmail.com>
2023-01-01 18:11:49 +0530

    init project

---
+++ src/main/java/com/vr/jpservice/JpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vr.jpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpserviceApplication.class, args);
+	}
+
+}
